28d40bf333705f1c7f61465d9cd0c268a716486f
Alex <gurievalex78@gmail.com>
2020-08-30 14:02:28 +0200

    adjustments

--- main.js
+++ main.js
@@ -4,23 +4,21 @@ $(document).ready(function() {
 			url: "https://flynn.boolean.careers/exercises/api/array/music",
 			method: "GET",
 			success: function (risultato) {
-				$("#risultati").html(risultato);
-				console.log(risultato.response); 
+				/* console.log(risultato.response);  */
 				var source = $("#entry-template").html();
-				console.log(source);
+				/* console.log(source); */
 				var template = Handlebars.compile(source);
-				console.log(template);
+				/* console.log(template); */
 
 				for(var i = 0; i < risultato.response.length; i++ ){
 					var a = risultato.response[i];
-
 					var html = template(a);
-					console.log(html);
+					/* console.log(html); */
 					$('.cds-container').append(html);
 				}
 
 			},
-			error: function (richiesta, stato, errori) {
+			error: function () {
 				alert("E' avvenuto un errore. " + errore);
 			}
 		}
