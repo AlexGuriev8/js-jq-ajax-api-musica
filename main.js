8ff91b25aa4f0c71e05a38f3e9522af46ee27b25
Alex <gurievalex78@gmail.com>
2020-08-31 10:21:28 +0200

    select

--- main.js
+++ main.js
@@ -4,14 +4,7 @@ $(document).ready(function() {
 			url: "https://flynn.boolean.careers/exercises/api/array/music",
 			method: "GET",
 			success: function (risultato) {
-				var source = $("#entry-template").html();
-				var template = Handlebars.compile(source);
-
-				for(var i = 0; i < risultato.response.length; i++ ){
-					var a = risultato.response[i];
-					var html = template(a);
-					$('.cds-container').append(html);
-				}
+			 insertCD(risultato);
 
 			},
 			error: function () {
@@ -19,4 +12,28 @@ $(document).ready(function() {
 			}
 		}
 	);
+	$('#scelta option').click(function(){
+		var genere = $(this).html();
+		console.log(genere);
+		//nascondere tutti gli elementi
+		//visualizzare gli elementi con la classe uguale alla option selezionata 
+		if(genere == 'All'){
+			$('.cd').show();
+		}else{
+			$('.cd').hide();
+		$('.cd.' + genere).show();
+		}
+	});
 });
+
+
+function insertCD (data){
+	var source = $("#entry-template").html();
+	var template = Handlebars.compile(source);
+
+	for (var i = 0; i < data.response.length; i++) {
+		var a = data.response[i];
+		var html = template(a);
+		$('.cds-container').append(html);
+	}
+}
